2f1345200a29a205585126df0b8d57e9585e51fd
Antonio Lav <antoniolavadodev@gmail.com>
2024-10-28 10:13:12 +0100

    feat: initial setup of springboot-crudJPA with basic configuration and structure

---
+++ springboot-crudjpa/src/test/java/com/alav/crudapp/springboot_crudjpa/SpringbootCrudjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alav.crudapp.springboot_crudjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
