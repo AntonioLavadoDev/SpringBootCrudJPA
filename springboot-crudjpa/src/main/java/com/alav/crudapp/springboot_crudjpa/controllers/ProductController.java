5f5d8d7a061a5778c021af5d7f7a550b98159f84
Antonio Lav <antoniolavadodev@gmail.com>
2024-10-29 17:30:08 +0100

    feat: add ProductController-in progress

---
+++ springboot-crudjpa/src/main/java/com/alav/crudapp/springboot_crudjpa/controllers/ProductController.java
@@ -0,0 +1,68 @@
+package com.alav.crudapp.springboot_crudjpa.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alav.crudapp.springboot_crudjpa.entities.Product;
+import com.alav.crudapp.springboot_crudjpa.services.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public List<Product> list() {
+        return service.findAll();
+        
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> view(@PathVariable Long id) {
+        Optional<Product> productOptional =  service.findById(id);
+        if(productOptional.isPresent()){
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Product product = new Product();
+        product.setId(id);
+        Optional<Product> productOptional =  service.delete(product);
+        if(productOptional.isPresent()){
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
+}
